ce6b40ea3d3c6636799f7cc6901e742a8370dade
Vadym Mariiechko <vadimich348@gmail.com>
2021-07-19 15:35:24 +0200

    Update DEBUG for production

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -2,7 +2,7 @@ import os
 from .base import *
 from ..env_utils import get_env
 
-DEBUG = False
+DEBUG = bool(get_env("DJANGO_DEBUG"))
 SECRET_KEY = get_env("DJANGO_SECRET_KEY")
 STATIC_URL = get_env("DJANGO_STATIC_URL")
 ALLOWED_HOSTS = get_env("DJANGO_ALLOWED_HOSTS").split(",")
