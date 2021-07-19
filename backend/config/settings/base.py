0e1e981054655453e21aa76982aec6739b373d22
Vadym Mariiechko <vadimich348@gmail.com>
2021-07-19 15:20:04 +0200

    Add static root folder directory

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -101,8 +102,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/django-static/")
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/django-static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
