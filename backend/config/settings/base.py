646d8fbb69686a2633f71c54a0d7917a0b0e0dc3
Vadym Mariiechko <vadimich348@gmail.com>
2021-07-19 15:27:40 +0200

    Enable cors headers for anyone

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -33,9 +33,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -110,3 +113,5 @@ STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/django-static/")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
