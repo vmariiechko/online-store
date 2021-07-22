4c53be36188e9ddfb7d4b289c668e8a932f15873
Vadym Mariiechko <vadimich348@gmail.com>
2021-07-22 17:33:41 +0200

    Add PostgreSQL migration

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,11 @@
 import os
 from .base import *
 from ..env_utils import get_env
+from dj_database_url import config
 
 DEBUG = bool(get_env("DJANGO_DEBUG"))
 SECRET_KEY = get_env("DJANGO_SECRET_KEY")
 STATIC_URL = get_env("DJANGO_STATIC_URL")
 ALLOWED_HOSTS = get_env("DJANGO_ALLOWED_HOSTS").split(",")
+
+DATABASES['default'].update(config(conn_max_age=600))
